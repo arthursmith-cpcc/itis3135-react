d5a072344a3f828e8a7b56061ac470ef53185717
arthursmith-cpcc <asmith62@email.cpcc.edu>
2026-03-25 22:57:45 -0400

    Updates to node pages
    1) Remove default html
    2) Create and import Header
    3) Create and import Footer

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <>
+            <footer>
+                <nav>Github</nav>
+                <p>Copyright &copy; 2026 | Page created by flair by Arthur Smith, certified in Responsive Web Design</p>
+            </footer>
+        </>
+    );    
+}
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <>
+            <header>
+                <h1>Arthur Smith's Ancient Sabertooth | ITIS3135</h1>
+                <nav>Home</nav>
+            </header>
+        </>
+    );
+}
