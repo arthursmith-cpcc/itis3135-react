f089250f51cad82e9cc51c601ad8a994284f05ff
Arthur <asmith62@email.cpcc.edu>
2026-03-28 10:33:20 -0400

    Gallery page changes
    1) Add gallery photos to assets folder
    2) Add placeholder css module file
    3) Add full gallery page html

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,5 +1,11 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
+import lodgeExterior from './assets/exterior.jpg'
+import lodgeGate from './assets/gate-by-Luke-Shaw.jpg'
+import lodgeBackLot from './assets/backlot-by-Marathon.jpg'
+import stoneArt from './assets/stone_art.jpg'
+import greenHotel from './assets/green-hotel.jpg'
+import mascotLogo from './assets/saber-tooth.svg'
 
 export default function Gallery() {
     return (
@@ -8,6 +14,29 @@ export default function Gallery() {
             <Header />
                 <main>
                     <h2>Gallery</h2>
+                    <img src={mascotLogo} alt="Sabertooth Lodge Logo" />
+                    <p>See our lodge for yourself!</p>
+                    <div id="photo-gallery">
+                        <figure class="photo">
+                            <img src={lodgeExterior} alt="Sabertooth Lodge Exterior" />
+                        </figure>
+                        <figure class="photo">
+                            <img src={lodgeGate} alt="Sabertooth Lodge Entrance Gate" />
+                            <figcaption><a href="https://www.geograph.org.uk/photo/7516302" target="_blank">Photo</a> &copy; Copyright <a href="https://www.geograph.org.uk/profile/129268" target="_blank">Luke Shaw</a> | <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">(cc-by-sa/2.0)</a> Creative Commons License.</figcaption>
+                        </figure>
+                        <figure class="photo">
+                            <img src={lodgeBackLot} alt="Sabertooth Lodge Back Lot" />
+                            <figcaption><a href="https://www.geograph.org.uk/photo/7594554" target="_blank">Photo</a> &copy; Copyright <a href="https://www.geograph.org.uk/profile/43806" target="_blank">Marathon</a> | <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">(cc-by-sa/2.0)</a> Creative Commons License.</figcaption>
+                        </figure>
+                        <figure class="photo">
+                            <img src={stoneArt} alt="Sabertooth Lodge Stone Art Installation" />
+                            <figcaption><a href="https://www.flickr.com/photos/slrjester/289603054/" target="_blank">Photo</a> &copy; Copyright <a href="https://www.flickr.com/photos/slrjester/" target="_blank">SLR Jester</a> | <a href="https://creativecommons.org/licenses/by/2.0/deed.en" target="_blank">Creative Commons License 2.0 Generic Deed</a></figcaption>
+                        </figure>
+                        <figure class="photo">
+                            <img src={greenHotel} alt="Sabertooth Lodge Second Exterior Shot" />
+                            <figcaption></figcaption>
+                        </figure>
+                    </div>
                 </main>
             <Footer />
         </>
