dfd6b1cdc1208ad5d47ee0bed93666ab6c709ad2
Arthur <asmith62@email.cpcc.edu>
2026-03-30 20:05:01 -0400

    Add Vercel link to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <>
             <footer>
                 <nav>
-                    <a href="https://github.com/arthursmith-cpcc" target="_blank">GitHub</a> | <a href="https://webpages.charlotte.edu/asmit775" target="_blank">CLTWeb</a> | <a href="https://arthursmith-cpcc.github.io" target="_blank">GitHub.io</a> | ITIS3135@Vercel | <a href="https://www.freecodecamp.org/arthursmith-uncc" target="_blank">freeCodeCamp</a> | <a href="https://www.linkedin.com/in/arthur-smith-1a2787101" target="_blank">LinkedIn</a>
+                    <a href="https://github.com/arthursmith-cpcc" target="_blank">GitHub</a> | <a href="https://webpages.charlotte.edu/asmit775" target="_blank">CLTWeb</a> | <a href="https://arthursmith-cpcc.github.io" target="_blank">GitHub.io</a> | <a href="https://itis3135-react-orcin.vercel.app" target="_blank">ITIS3135@Vercel</a> | <a href="https://www.freecodecamp.org/arthursmith-uncc" target="_blank">freeCodeCamp</a> | <a href="https://www.linkedin.com/in/arthur-smith-1a2787101" target="_blank">LinkedIn</a>
                 </nav>
                 <p>Copyright &copy; 2026 | Page created by <a href="https://webpages.charlotte.edu/asmit775/itis3135/flairbyarthursmith.com/index.html" target="_blank">flair by Arthur Smith</a>, certified in <a href="https://freecodecamp.org/certification/arthursmith-uncc/responsive-web-design-v9" target="_blank">Responsive Web Design</a></p>
             </footer>
